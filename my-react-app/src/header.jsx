0521a20b7a1d9f5ea0bcb7493ac4a69dcbf0c4ae
andreidanutmihai85 <andreidanutmihai85@gmail.com>
2024-10-20 19:56:37 +0300

    inceput

---
+++ my-react-app/src/header.jsx
@@ -0,0 +1,13 @@
+function Header (){
+    return(
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <li> Home</li>
+                <li> About</li>
+            </nav>
+            <button>Click Me</button>
+        </header>
+    )
+}
+export default Header
